b11039bfcb302344e39b77dd71d61dd8c01a12e3
Tuan Duong <bacduong@gmail.com>
2014-08-09 00:54:16 +0700

    Exercise 02-store-users-into-database

---
+++ exercises/02-create-user-and-store-in-database/users.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `users` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `fullname` VARCHAR(45) NULL,
+  `email` VARCHAR(45) NULL,
+  `age` INT NULL,
+  `gender` VARCHAR(20) NULL,
+  PRIMARY KEY (`id`));
